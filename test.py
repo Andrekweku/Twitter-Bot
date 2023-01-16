244b8dd8c18e87426d5079d23554cf058f52355a
Kweku <aetroo@gmail.com>
2023-01-16 20:50:00 +0000

    first commit

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import gspread
 from twitter import *
+from config import settings
 
 
-consumer_key = "eHPKezxc1UKHOcn9s520MiPJB"
-consumer_secret = "nVhkh9xEJQVLmpaV8IOPQ4w1Tj9v2xTmiX8ZZvmrmXsHVOpg6x"
-token = "1530015028631445505-YLnB5n92Sxb9bjc3AsvVdueZ8ZmpWW"
-token_secret = "XbJzvRmgwl5lcDJFhbWwFmEn0rra6fVI1OusyBUatKF4k"
+consumer_key = settings.consumer_key
+consumer_secret = settings.consumer_secret
+token = settings.token
+token_secret = settings.token_secret
 
-def test(event=None, context=None):
+def post_tweet(event=None, context=None):
 
     gc = gspread.service_account('credentials.json')
 
     t = Twitter(
         auth=OAuth(token, token_secret, consumer_key, consumer_secret))
+
 
     # Open a sheet from a spreadsheet in one go
     wks = gc.open("kweku").sheet1
@@ -24,7 +26,9 @@ def test(event=None, context=None):
     t.statuses.update(
         status=next_tweet)
 
+
     # #Delete current cell and move to next cell
     wks.delete_rows(2)
 
-test()
+
+post_tweet()
